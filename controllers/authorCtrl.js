127a7f1b75db7df5aeea424673880bb23146f6a0
gary <ggrumbley@ucsd.edu>
2017-02-15 15:21:44 -0800

    Edit and delete buttons added to detail pages

--- controllers/authorCtrl.js
+++ controllers/authorCtrl.js
@@ -65,11 +65,44 @@ exports.author_create_post = (req, res, next) => {
 }
 
 exports.author_delete_get = (req, res, next) => {
-  res.send('Auther delete GET')
+  async.parallel({
+    author: (f) => Author.findById(req.params.id).exec(f),
+    authors_books: (f) => Book.find({ 'author': req.params.id }).exec(f),
+  },
+
+  (err, results) => {
+    if (err) { return next(err) }
+    res.render('author_delete', {
+      title: 'Delete Author',
+      author: results.author,
+      author_books: results.authors_books,
+    })
+  })
 }
 
 exports.author_delete_post = (req, res, next) => {
-  res.send('Auther delete POST')
+  req.checkBody('authorid', 'Author id must exist').notEmpty()
+
+  async.parallel({
+    author: (f) => Author.findById(req.body.authorid).exec(f),
+    authors_books: (f) => Book.find({ 'author': req.body.authorid }, 'title summary').exec(f),
+  },
+  (err, results) => {
+    if (err) { return next(err) }
+    if (results.authors_books > 0) {
+      res.render('author_delete', {
+        title: 'Delete Author',
+        author: results.author,
+        author_books: results.authors_books
+      })
+      return
+    } else {
+      Author.findByIdAndRemove(req.body.authorid, (err) => {
+        if (err) { return next(err) }
+        res.redirect('/catalog/authors')
+      })
+    }
+  })
 }
 
 exports.author_update_get = (req, res, next) => {
